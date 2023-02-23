b0df14af863899fb70d8278b15be62d2c9c01ab8
José Ramos <02.jrsramos@gmail.com>
2023-02-23 13:34:32 -0300

    v1.3

--- react-form/src/components/Thanks.jsx
+++ react-form/src/components/Thanks.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import "./Thanks.css";
 
-const Thanks = () => {
+import {BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill, BsEmojiNeutralFill, BsFillEmojiFrownFill, BsFillEmojiNeutralFill} from "react-icons/bs";
+
+const Thanks = ({data}) => {
     return ( 
-        <div>
-            <h2>Thanks</h2>
+        <div className="thanks-container">
+            <h2>Falta pouco...</h2>
+            <p>A sua opinião é muito importante, em breve você receberá um cupom de 10% de desconto para a sua próxima compra.</p>
+            <p>Para concluir sua avaliação clique no botão de enviar abaixo.</p>
+            <h3>Aqui está o resumo da sua avaliação:
+                <p className='review-data'>
+                    <span>Satisfação com o produto:</span>
+                </p>
+                <p className="review-data">
+                    <span>Comentário:</span>
+                </p>
+            </h3>
         </div>
      );
 }
