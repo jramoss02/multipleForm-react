96a6036bd23487b55eea0f959953563938e93cf2
José Ramos <02.jrsramos@gmail.com>
2023-02-23 11:50:12 -0300

    app-v1.1

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -7,6 +7,7 @@ import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
 
 import {useForm} from "./hooks/useForm";
+import Steps from "./components/Steps";
 
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
         <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
       </div>
       <div className="form-container">
-        <p>Etapas</p>
+        <Steps currentStep={currentStep}/>
         <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
