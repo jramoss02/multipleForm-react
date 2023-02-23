a92673fe6ee8e14fea4761c355a1cdfaffa71f08
José Ramos <02.jrsramos@gmail.com>
2023-02-23 08:22:59 -0300

    Update UserForm.jsx

--- react-form/src/components/UserForm.jsx
+++ react-form/src/components/UserForm.jsx
@@ -7,9 +7,19 @@ const UserForm = () => {
                 <label htmlFor="name">Nome:</label>
                 <input
                     type="text"
-                    name='text'
+                    name='name'
                     id='name'
                     placeholder='Digite seu nome'
+                    required
+                />
+            </div>
+            <div className="form-control">
+                <label htmlFor="email">E-mail:</label>
+                <input
+                    type="email"
+                    name='email'
+                    id='email'
+                    placeholder='Digite seu email'
                     required
                 />
             </div>
