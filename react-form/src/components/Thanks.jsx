e983a8e3a2ea7bed89cbbd2152aee464a68fbe43
José Ramos <02.jrsramos@gmail.com>
2023-02-28 08:57:27 -0300

    Update Thanks.jsx

--- react-form/src/components/Thanks.jsx
+++ react-form/src/components/Thanks.jsx
@@ -3,20 +3,28 @@ import "./Thanks.css";
 
 import {BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill, BsEmojiNeutralFill, BsFillEmojiFrownFill, BsFillEmojiNeutralFill} from "react-icons/bs";
 
+const emojiData = {
+    unsatisfied: <BsFillEmojiFrownFill/>,
+    neutral: <BsFillEmojiNeutralFill/>,
+    satisfied: <BsFillEmojiSmileFill/>,
+    very_satisfied: <BsFillEmojiHeartEyesFill/>,
+}
+
 const Thanks = ({data}) => {
     return ( 
         <div className="thanks-container">
             <h2>Falta pouco...</h2>
             <p>A sua opinião é muito importante, em breve você receberá um cupom de 10% de desconto para a sua próxima compra.</p>
             <p>Para concluir sua avaliação clique no botão de enviar abaixo.</p>
-            <h3>Aqui está o resumo da sua avaliação:
+            <h3>Aqui está o resumo da sua avaliação {data.name}:</h3>
                 <p className='review-data'>
                     <span>Satisfação com o produto:</span>
+                    {emojiData[data.review]}
                 </p>
                 <p className="review-data">
                     <span>Comentário:</span>
+                    {data.comment}
                 </p>
-            </h3>
         </div>
      );
 }
